f9e78c6acb0e7447e3af85e6a779aa9e654481a2
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-09-08 21:13:51 +0200

    Make Navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import style from "./css/Navbar.module.css";
+
 const Navbar = () => {
   return (
-    <nav className="bg-accent color-white flex justify-space-between p-1">
-      <h1 className="text-bold m-0 font-size-md">movie-db</h1>
+    <nav>
+      <div className={style.navbar}>
+        <h1 className="text-bold m-0 font-size-md">movie-db</h1>
 
-      <ul className="list-style-none my-0 pl-0 flex gap-2">
-        <li>
-          <NavLink to="/now-playing" className="color-white">
-            Now Playing
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/popular" className="color-white">
-            Popular
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/top-rated" className="color-white">
-            Top Rated
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/genres" className="color-white">
-            Genres
-          </NavLink>
-        </li>
-      </ul>
+        <ul className="list-style-none my-0 pl-0 flex gap-2">
+          <li>
+            <NavLink to="/now-playing" className="color-white">
+              Now Playing
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/popular" className="color-white">
+              Popular
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/top-rated" className="color-white">
+              Top Rated
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/genres" className="color-white">
+              Genres
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className={style.spacer}></div>
     </nav>
   );
 };
