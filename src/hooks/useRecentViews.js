84e3b755fcd1441d9491bcdd33c66a9c5c744531
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-09-15 22:39:26 +0200

    Avoid duplicates in recent views

--- src/hooks/useRecentViews.js
+++ src/hooks/useRecentViews.js
@@ -10,8 +10,20 @@ export const useRecentViews = () => {
 
   const addMovie = (movie) => {
     const recent = JSON.parse(localStorage.getItem("recentViews")) || [];
-    if (recent.length >= 10) recent.pop();
-    recent.unshift(movie);
+
+    const index = recent.findIndex((val) => val.id === movie.id);
+    if (index >= 0) {
+      // Movie is already in list
+      // Move it to the front
+      recent.unshift(recent.splice(index, 1)[0]);
+    } else {
+      // Movie is not in list
+      // If list is full, remove the oldest
+      if (recent.length >= 10) recent.pop();
+      // Add new movie to front
+      recent.unshift(movie);
+    }
+
     localStorage.setItem("recentViews", JSON.stringify(recent));
     setMovies(recent);
   };
