70c4662a4c84c3a9788278439357b0571fa3b9f0
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-09-15 11:18:57 +0200

    Rename variables more descriptively

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -12,33 +12,35 @@ import style from "./css/MoviePage.module.css";
 const MoviePage = () => {
   const { id } = useParams();
 
-  const { data } = useQuery({
+  const { data: movie } = useQuery({
     queryKey: ["movie", id],
     queryFn: () => getMovieById(id),
   });
 
-  if (!data) return null;
+  if (!movie) return null;
 
-  const cast = data.credits.cast;
-  const movies = data.recommendations.results;
+  const cast = movie.credits.cast;
+  const recommendations = movie.recommendations.results;
 
   return (
     <div className={`${style.moviePage} color-white`}>
       <div className={style.backdrop}>
-        {data.backdrop_path && <img src={imgUrl.large + data.backdrop_path} />}
+        {movie.backdrop_path && (
+          <img src={imgUrl.large + movie.backdrop_path} />
+        )}
       </div>
 
       <main>
         <div className={style.topSection}>
-          {data.poster_path && (
-            <img src={imgUrl.small + data.poster_path} className="rounded" />
+          {movie.poster_path && (
+            <img src={imgUrl.small + movie.poster_path} className="rounded" />
           )}
 
           <div>
-            <h2 className="font-size-xxl mb-1">{data.title}</h2>
+            <h2 className="font-size-xxl mb-1">{movie.title}</h2>
 
             <div className="flex flex-wrap gap-05">
-              {data.genres.map((genre, i) => (
+              {movie.genres.map((genre, i) => (
                 <span key={i} className="round bg-dark px-1 py-05">
                   {genre.name}
                 </span>
@@ -47,7 +49,7 @@ const MoviePage = () => {
           </div>
         </div>
 
-        {data.overview && <p>{data.overview}</p>}
+        {movie.overview && <p>{movie.overview}</p>}
 
         {cast?.length > 0 && (
           <>
@@ -62,10 +64,10 @@ const MoviePage = () => {
           </>
         )}
 
-        {movies?.length > 0 && (
+        {recommendations?.length > 0 && (
           <>
             <h3 className="text-center">Recommended</h3>
-            <MovieList movies={movies.slice(0, 8)} />
+            <MovieList movies={recommendations.slice(0, 8)} />
           </>
         )}
       </main>
