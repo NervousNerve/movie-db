7445184b7cb40f94b0abad5dca0c00c4a95191c0
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-09-15 13:25:09 +0200

    Fix typo in url

--- src/components/ActorCard.jsx
+++ src/components/ActorCard.jsx
@@ -8,12 +8,12 @@ import style from "./css/ActorCard.module.css";
 const ActorCard = ({ actor }) => {
   return (
     <div className="flex gap-1">
-      <Link to={"/actor/" + actor.id} className={style.picture}>
+      <Link to={"/actors/" + actor.id} className={style.picture}>
         {actor.profile_path && <img src={imgUrl.small + actor.profile_path} />}
       </Link>
 
       <div className={style.info}>
-        <Link to={"/actor/" + actor.id} className="m-0 color-white">
+        <Link to={"/actors/" + actor.id} className="m-0 color-white">
           {actor.name}
         </Link>
         <p className="m-0 color-dark">{actor.character}</p>
