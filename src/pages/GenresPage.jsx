23e5a3bfc30aad0cf85ddef2854371dce965ceb5
Sebastian Mineur <sebastian.mineur@gmail.com>
2021-09-19 18:06:04 +0200

    Shorten query key

--- src/pages/GenresPage.jsx
+++ src/pages/GenresPage.jsx
@@ -24,7 +24,7 @@ const GenresPage = () => {
   });
 
   const { data: movies } = useQuery({
-    queryKey: ["movies", "genres", genres.join(), page],
+    queryKey: ["with_genres", genres.join(), page],
     queryFn: () => getMoviesByGenres(genres.join(), page),
   });
 
